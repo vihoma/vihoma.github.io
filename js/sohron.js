7ea56b0960ed488dece779bf1db2efc79ab148b4
Marko Vihoma <marko.vihoma@pp1.inet.fi>
2014-02-04 16:07:54 +0200

    The FSCK'in Time Machine SCREW-UP!

---
+++ js/sohron.js
@@ -0,0 +1,11 @@
+var red = [0, 100, 63];
+var purple = [280, 50, 60];
+var white = [256, 256, 256]
+
+var myName = "Sohron";
+//var letterColors = [red, purple];
+var letterColors = [red, white];
+
+bubbleShape = "square";
+drawName(myName, letterColors);
+bounceBubbles();
